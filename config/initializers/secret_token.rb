424878fa9b0f51e6dcafa516416668880118fb3b
Ilker Inanc <ilkerinancc@gmail.com>
2012-11-29 00:56:43 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EtInterface::Application.config.secret_token = 'bbff18db90649593fb16cadca6daee43c296b5d325bc3072008ed16143578d8e5e146752e83704f6d10db2f3b5d7343c70761807b0a6d9a49b4169e67071d2b4'
